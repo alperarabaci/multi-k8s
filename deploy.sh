6cf41b4d1600172e4d0863eeaaed6913f02d81cd
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-02-27 10:15:40 +0300

    minor fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t alperarabaci/multi-client:latest -t alperarabaci/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t alperarabaci/multi-server:latest -t alperarabaci/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t alperarabaci/multi-worker:latest -t alperarabaci/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
+docker build -t alperarabaci/multi-worker:latest -t alperarabaci/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push alperarabaci/multi-client:latest
 docker push alperarabaci/multi-server:latest
 docker push alperarabaci/multi-worker:latest
@@ -10,6 +11,6 @@ docker push alperarabaci/multi-server:$SHA
 docker push alperarabaci/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment  server=alperarabaci/multi-server:$SHA
-kubectl set image deployments/client-deployment  server=alperarabaci/multi-client:$SHA
-kubectl set image deployments/worker-deployment  server=alperarabaci/worker-client:$SHA
+kubectl set image deployments/server-deployment server=alperarabaci/multi-server:$SHA
+kubectl set image deployments/client-deployment client=alperarabaci/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=alperarabaci/multi-worker:$SHA
